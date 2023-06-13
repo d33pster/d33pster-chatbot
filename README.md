11436513387338ed9b52479f3618bc63a9d946ee
d33pster <deep.main.ac@gmaiil.com>
2023-06-13 06:55:19 +0530

    d33pster v1.5
    ~~change-log~~
        ~ added showing projects option

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# d33pster-chatbot v1.0
+# d33pster-chatbot v1.5
 Contains an AI version of me
 ## Prereqs ~~ Annaconda preinstalled
 ## Instructions
 ### git clone the repo
-### go to the cloned folder
-### for Windows run command "python d33pster.py"
-### for Linux first run "chmod +x ./d33pster.py" and then "./d33pster.py"
+### Open two terminals and go to the cloned folder in both of them
+### for Windows run command "python d33pster.py" in one terminal and run "rasa run actions" in the other
+### for Linux first run "chmod +x ./d33pster.py" and then "./d33pster.py" in one terminal and "rasa run actions" in the other
--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,40 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+projects = {"covid": "https://github.com/d33pster/COVID-19-DL-ChestXray",
+            "WebRep": "https://github.com/d33pster/WebRep",
+            "PSSQL-auto": "https://github.com/d33pster/PSSQL-auto",
+            "d33pster-chatbot": "https://github.com/d33pster/d33pster-chatbot"}
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_show_projects"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        for key in projects:
+            if key=='covid':
+                output = "Deep Learning Based COVID-19 detection using chest Xrays: " + projects[key]
+                dispatcher.utter_message(text=output) 
+            elif key=='WebRep':
+                output = "WebRep ~ A cross platform python app to recon a website/webapp: " + projects[key]
+                dispatcher.utter_message(text=output) 
+            elif key=='PSSQL-auto':
+                output = "PSSQL-auto ~ A cross platform python app to process Big Data: " + projects[key]
+                dispatcher.utter_message(text=output) 
+            elif key=='d33pster-chatbot':
+                output = "Me: " + projects[key]
+                dispatcher.utter_message(text=output)
+            else:
+                pass
+                
+        return []
